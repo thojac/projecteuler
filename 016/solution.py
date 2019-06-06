6aae96d85af92fbaf0d12f6e5d34a9f1def5c722
thojac <johntj@ifi.uio.no>
2019-06-06 19:44:41 +0200

    Restructured by nesting folder

--- 016/solution.py
+++
@@ -1,7 +0,0 @@
-# Problem  016 - Power digit sum
-
-def power_digit_sum(x, y):
-    return sum(int(i) for i in list(str(pow(x,y))))
-
-if __name__ == "__main__":
-    print(power_digit_sum(2, 1000))
