5537453ea3302a870745c08bdba5192a836f2fa1
thojac <johntj@ifi.uio.no>
2019-06-17 23:21:10 +0200

    didn't manage to figure out a smart way to generate all cyclic primes

--- python/p026.py
+++ python/p026.py
@@ -1,27 +1,64 @@
 # Problem  026 - Reciprocal cycles
 
-import string
-from decimal import *
+from eulerlib import generate_primes
 
-getcontext().prec = 50
-
-text = "ababababababbababbaabababbababbabab"
-pattern = "abbabab"
-alphabet = list(string.ascii_lowercase)
-
-print(boyer_moore(text, pattern, alphabet))
+"""
+Note: This task could be solved faster by looping over a list of all
+cyclic prime numbers. But then cycle_length would be easier to write,
+and i would have to manually create a list containing them
+"""
 
 
+def is_cyclic_prime(prime):
+    for n in str(prime):
+        if n in ['0', '2', '4', '5', '6', '8']:
+            return False
+    return True
 
-def get_decimals_as_tuple(n):
-    return (Decimal(1) / Decimal(n)).as_tuple()
+def is_full_reptend_prime(p):
+    k = p - 1
+    print(p, '\t', 10**k, 10**k / p, "==", 1 % p)
+    return 10**k == 1
 
 
-def cycles():
+def generate_decimals_iterator_2(n):
+    for i in iter(int, 1):
+        yield 10**i % n
+
+
+def generate_decimal_iterator(denominator):
+    numerator = 10
+    while numerator != 0:
+        yield numerator // denominator
+        numerator = (numerator % denominator) * 10
+
+# Not working on quite a few numbers
+def _cycle_length(n):
+    iter_decimal = generate_decimal_iterator(n)
+
+    # skip first, as this is often 1 - aka terminating cond
+    count = -1
+    prev = -1
+    for dec in iter_decimal:
+        count += 1
+        #print("dec:\t\t", dec)
+        if (dec == 1 or dec == 0) and count > 1:
+            return count
+        elif prev == dec:
+            # Same num inf
+            return 1
+        prev = dec
+
+    # iterator finished- without reaching repeating patter
+    return 0
+
+
+def solution():
     pass
 
 
 if __name__ == "__main__":
-    pass
-#    for i in range(1, 100):
-#        print(i, get_decimals_as_tuple(i).digits)
+    primes = generate_primes(20)
+    cyclic_primes = list(filter(lambda x: is_full_reptend_prime(x), primes))
+    print(primes)
+    print(cyclic_primes)
